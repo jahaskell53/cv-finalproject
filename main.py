9dc48e7d9af5297ac6902b434f6f499bc067b2fa
Ayma-n <aymanbt14@gmail.com>
2022-11-28 16:52:54 -0500

    font size now adjusts based on box height

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import keras_ocr
 import matplotlib.pyplot as plt
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageTransform, ImageFont
 import numpy as np
 from random import randint
 from math import sqrt
 
 pipleline = keras_ocr.pipeline.Pipeline()
-images = [keras_ocr.tools.read('test.png'), keras_ocr.tools.read('stop.jpeg')]
+images = [keras_ocr.tools.read('test.png'), keras_ocr.tools.read('billboard.jpg')]
 
 prediction_groups = pipleline.recognize(images)
 fig, axs = plt.subplots(nrows=len(images), figsize=(10, 20))
@@ -19,7 +19,7 @@ for ax, image, predictions in zip(axs, images, prediction_groups):
 
 print(prediction_groups[1])
 
-pillow_image = Image.open("stop.jpeg")
+pillow_image = Image.open("billboard.jpg")
 
 for text, box in prediction_groups[1]:
     print("hello")
@@ -32,8 +32,28 @@ for text, box in prediction_groups[1]:
     width = int(sqrt((tright[0] - tleft[0])**2 + (tright[1] - tleft[1])**2))
     height = int(sqrt((tright[0] - bright[0])**2 + (tright[1] - bright[1])**2))
     overlay_img = Image.new('RGB', (width, height), color='blue')
+
+    # Determine adequate font size
+    font_size = 1
+    font = ImageFont.truetype("Arial.ttf", font_size)
+    max_fraction = 0.90
+
+    breakpoint = max_fraction * height
+    jumpsize = 75
+    while True:
+        #print("hello")
+        if font_size < breakpoint:
+            font_size += jumpsize
+            print(font_size)
+        else:
+            jumpsize = jumpsize // 2
+            font_size -= jumpsize
+        font = ImageFont.truetype("arial.ttf", font_size)
+        if jumpsize <= 1:
+            break
+    
     imgDraw = ImageDraw.Draw(overlay_img)
-    imgDraw.text((10, 10), text, fill=(255, 255, 0))
+    imgDraw.text((0, 0), text, fill=(255, 255, 0), font=font)
 
     test_int = randint(1, 100)
 
